6385a8f24cd6e05f4a62f9884d71e46430c69927
Rémi Bleuse <remi.bleuse@gmail.com>
2022-02-03 09:57:18 +0900

    format files

--- src/main/kotlin/org/rbleuse/betterreadsdataloaderkotlin/BetterReadsDataLoaderKotlinApplication.kt
+++ src/main/kotlin/org/rbleuse/betterreadsdataloaderkotlin/BetterReadsDataLoaderKotlinApplication.kt
@@ -8,7 +8,6 @@ import org.rbleuse.betterreadsdataloaderkotlin.dto.AuthorDumpDto
 import org.rbleuse.betterreadsdataloaderkotlin.dto.BookDumpDto
 import org.rbleuse.betterreadsdataloaderkotlin.repository.AuthorRepository
 import org.rbleuse.betterreadsdataloaderkotlin.repository.BookRepository
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
